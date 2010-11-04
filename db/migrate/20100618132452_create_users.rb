b5580b3d4c07ff1f96612eb8f5cc6e8e8bdce3ea
Tapio Auvinen <tsauvine@cc.hut.fi>
2010-11-04 15:44:08 +0200

    Added reverse migration to revert from Devise to the old system.

--- db/migrate/20100618132452_create_users.rb
+++ db/migrate/20100618132452_create_users.rb
@@ -18,5 +18,23 @@ class CreateUsers < ActiveRecord::Migration
 
   def self.down
     drop_table :users
+    
+    create_table :users do |t|
+      t.string :login,                :null => false
+      t.string :crypted_password,     :limit => 40
+      t.string :salt,                 :limit => 40
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.boolean :admin
+      t.string :locale
+      t.integer :thumbs_up, :default => 0
+      t.integer :thumbs_down, :default => 0
+      t.string :remember_token
+      t.datetime :remember_token_expires_at
+      t.timestamp :last_login_at
+      t.timestamp :previous_login_at
+      t.timestamps
+    end
   end
 end

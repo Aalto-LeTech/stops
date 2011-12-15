32002c6703c5866186dfe2f529d09d9467f33cbf
Tapio Auvinen <tapio.auvinen@aalto.fi>
2011-12-15 05:25:58 +0200

    Added a visualization of the graph

---
+++ public/javascripts/graphCourse.js
@@ -0,0 +1,298 @@
+function GraphLevel(level_number) {
+  this.number = level_number;
+  this.courses = [];
+  this.courseMargin = 20;
+  this.height = 0;
+}
+
+GraphLevel.prototype.addCourse = function(course) {
+  this.courses.push(course);
+}
+
+GraphLevel.prototype.getCourses = function(course) {
+  return this.courses;
+}
+
+GraphLevel.prototype.getWidth = function(course) {
+  return this.courses.length;
+}
+
+GraphLevel.prototype.updateHeight = function(course) {
+  this.height = 0;
+
+  for (var course_index in this.courses) {
+    var course = this.courses[course_index];
+    this.height += course.getElement().height() + this.courseMargin;
+  }
+
+  return this.height;
+}
+
+GraphLevel.prototype.setYindicesBackwards = function() {
+  // Set each course to the barycenter of the forward neighbors
+  for (var course_index in this.courses) {
+    var course = this.courses[course_index];
+    var visibleNeighbors = 0;
+
+    // Calculate average of the y coordinates of the forward neighbor
+    var y = 0.0;
+    for (var neighbor_index in course.prereqTo) {
+      var neighbor = course.prereqTo[neighbor_index];
+      if (neighbor.visible) {
+        y += neighbor.y;
+        visibleNeighbors++;
+      }
+    }
+
+    if (course.prereqTo.length > 0) {
+      y /= visibleNeighbors;
+    } else {
+      y = this.height / 2.0;
+    }
+  }
+
+  this.distributeCoursesEvenly();
+}
+
+GraphLevel.prototype.distributeCoursesEvenly = function() {
+  // Sort courses by Y
+  this.courses.sort(function(a,b){b.y - a.y});
+
+  // Distribute evenly
+  //var step = (this.maxHeight - this.height) / (this.courses.length - 1)
+  var y = this.maxHeight / 2.0 - this.height / 2;
+  for (var course_index in this.courses) {
+    var course = this.courses[course_index]
+    course.y = y;
+    y += course.getElement().height() + this.courseMargin;
+  }
+}
+
+
+function GraphCourse(id, code, name) {
+  this.element = false;
+  this.id = id;
+  this.code = code;
+  this.name = name;
+
+  this.level = 0;
+  this.x = 0;
+  this.y = 0;
+
+  this.skills = [];
+  this.prereqs = [];
+  this.prereqTo = [];
+
+  this.visible = false;
+  this.visibleNeighborsForward = 0;
+
+//   this.visited = false;
+//   this.prereqs = {};           // Prerequisite courses. courseCode => course object
+//   this.prereqTo = {};          // Courses for which this course is a prereq. courseCode => course object
+//   this.prereqsCount = 0;
+//   this.prereqToCount = 0;
+
+  this.x = 0;
+  this.y = 0;
+
+};
+
+GraphCourse.prototype.addSkill = function(skill) {
+  this.skills.push(skill);
+}
+
+GraphCourse.prototype.addPrereq = function(course) {
+  this.prereqs.push(course);
+  course.prereqTo.push(this);
+}
+
+/**
+ * Returns a div.
+ **/
+GraphCourse.prototype.getElement = function(view) {
+  if (this.element) {
+    return this.element;
+  }
+
+  var div = $('<div class="course"><h1>' + this.code + ' ' + this.name + '</h1></div>');
+  var ul = $('<ul />');
+  div.append(ul);
+
+  for (var array_index in this.skills) {
+    var skill = this.skills[array_index];
+    skill.view = view;
+    var li = $('<li>' + skill.description + '</li>');
+    skill.element = li;
+    ul.append(li);
+    li.click($.proxy(skill.click, skill));
+  }
+
+  this.element = div;
+
+  return div;
+}
+
+GraphCourse.prototype.setPosition = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.element.offset({ left: this.x, top: this.y });
+}
+
+GraphCourse.prototype.updatePosition = function() {
+  this.element.offset({ left: this.x, top: this.y });
+}
+
+// GraphCourse.prototype.calculateVisibleNeighbors = function() {
+//   for (var neighbor_index in course.prereqTo) {
+//     var neighbor = course.prereqTo[neighbor_index];
+//     if (neighbor.visible) {
+//       this.visibleNeighborsForward++;
+//     }
+//   }
+// }
+
+
+// GraphCourse.prototype.getPrereqs = function() {
+//   return this.prereqs;
+// }
+//
+// /**
+//  * Adds a prerequisite course. This course is automatically added to the "prerequisite to" list of the other course.
+//  */
+// GraphCourse.prototype.addPrereq = function(other) {
+//   this.prereqs[other.code] = other;
+//   other.prereqTo[this.code] = this;
+// }
+//
+//
+// GraphCourse.prototype.calculatePaths = function() {
+//   this.dfsForward();
+//   this.dfsBackward();
+// }
+//
+// GraphCourse.prototype.render = function(c, view) {
+//   // Box
+//   c.strokeStyle = "#808080";
+//   c.fillStyle = "#f0f0f0";
+//   c.lineWidth = 1;
+//   c.fillRect(this.x, this.y, view.courseWidth, view.courseHeight);
+//   c.strokeRect(this.x, this.y, view.courseWidth, view.courseHeight);
+//
+//   // Texts
+//   c.font = "10px sans-serif"
+//   c.fillStyle = "#000000";
+//   c.textBaseline = "top";
+//   c.fillText(this.code, this.x + 2, this.y + 2);
+// }
+//
+// GraphCourse.prototype.renderForward = function(c, view, depth) {
+//   if (!this.onPath || this.visited) {
+//     return;
+//   }
+//
+//   this.visited = true;
+//
+//   // Visit neighbors
+//   var requiredSpace = this.prereqToCount * (view.courseWidth + 10);
+//   var i = 0;
+//   for (array_index in this.prereqTo) {
+//     var other = this.prereqTo[array_index];
+//
+//     // Position neighbor
+//     if (!other.visited && other.onPath) {
+//       //other.x = this.x - requiredSpace / 2 + i * (view.courseWidth + 10);
+//     }
+//
+//     other.renderForward(c, view, depth+1);
+//     i++;
+//   }
+//
+//   // Arrows
+//   if (depth == 1) {
+//     c.strokeStyle = "#808080";
+//   } else {
+//     c.strokeStyle = "#ddd";
+//   }
+//   c.lineWidth = 1;
+//   c.beginPath();
+//   for (var index in this.prereqTo) {
+//     var other = this.prereqTo[index];
+//
+//     c.moveTo(this.x + view.courseWidth / 2, this.y + view.courseHeight);
+//     c.lineTo(other.x + view.courseWidth / 2, other.y);
+//     //c.fillText(other.code, other.x + view.courseWidth / 2, other.y-20);
+//     //c.fillText(other.period, other.x + view.courseWidth / 2, other.y-10);
+//
+//   }
+//   c.stroke();
+//
+//   this.render(c, view);
+// }
+//
+// GraphCourse.prototype.renderBackward = function(c, view, depth) {
+//   // Visit
+//   if (!this.onPath || this.visited) {
+//     return;
+//   }
+//
+//   this.visited = true;
+//
+//   // Visit neighbors
+//   var requiredSpace = this.prereqsCount * (view.courseWidth + 10);
+//   var i = 0;
+//   for (array_index in this.prereqs) {
+//     var other = this.prereqs[array_index];
+//
+//     // Position neighbor
+//     if (!other.visited && other.onPath) {
+//       //other.x = this.x - requiredSpace / 2 + i * (view.courseWidth + 10);
+//     }
+//
+//     other.renderBackward(c, view,depth+1);
+//     i++;
+//   }
+//
+//   // Arrows
+//   if (depth == 1) {
+//     c.strokeStyle = "#808080";
+//   } else {
+//     c.strokeStyle = "#ddd";
+//   }
+//   c.lineWidth = 1;
+//   c.beginPath();
+//
+//
+//   for (var index in this.prereqs) {
+//     var other = this.prereqs[index];
+//
+//     c.moveTo(this.x + view.courseWidth / 2, this.y);
+//     c.lineTo(other.x + view.courseWidth / 2, other.y + view.courseHeight);
+//   }
+//   c.stroke();
+//
+//   this.render(c, view);
+// }
+//
+// GraphCourse.prototype.dfsForward = function(c) {
+//   // Visit
+//   this.onPath = true;
+//
+//   // Visit neighbors
+//   for (array_index in this.prereqTo) {
+//     var other = this.prereqTo[array_index];
+//     other.dfsForward();
+//     this.prereqToCount++;
+//   }
+// }
+//
+// GraphCourse.prototype.dfsBackward = function(c) {
+//   // Visit
+//   this.onPath = true;
+//
+//   // Visit neighbors
+//   for (array_index in this.prereqs) {
+//     var other = this.prereqs[array_index];
+//     other.dfsBackward();
+//     this.prereqsCount++;
+//   }
+// }
---
+++ public/javascripts/skillGraphView.js
@@ -0,0 +1,318 @@
+
+var skillGraphView = {
+  //svgNS: "http://www.w3.org/2000/svg",
+  courses: {},  // id -> course object
+  skills: {},   // id -> skill object
+
+  initialize: function() {
+    this.paper = Raphael(document.getElementById('svg'), 100, 100);
+  },
+
+  load: function(coursesPath, skillsPath) {
+    $.ajax({
+      url: coursesPath,
+      context: this,
+      dataType: 'json',
+      success: this.loadCourses,
+      async: false
+    });
+
+    $.ajax({
+      url: skillsPath,
+      context: this,
+      dataType: 'json',
+      success: this.loadSkills,
+      async: false
+    });
+  },
+
+  /**
+   * Loads courses from JSON data.
+   */
+  loadCourses: function(data) {
+
+//           $.each(data.items, function(i,item){
+//             $("<img/>").attr("src", item.media.m).appendTo("#images");
+//             if ( i == 3 ) return false;
+//           });
+
+    for (var array_index in data) {
+      var rawData = data[array_index].scoped_course;
+
+      //var periodId = parseInt(rawData.period_id);
+      var course = new GraphCourse(rawData.id, rawData.code, rawData.translated_name);
+      this.courses[rawData.id] = course;
+    }
+
+    // Set connections between courses
+    for (var array_index in data) {
+      rawData = data[array_index].scoped_course;
+      course = this.courses[rawData.id];
+
+      for (var array_index2 in rawData.strict_prereq_ids) {
+        var prereq = this.courses[rawData.strict_prereq_ids[array_index2]];
+
+        if (prereq) {
+          course.addPrereq(prereq);
+        }
+      }
+    }
+
+
+    /*
+    this.periodHeight = this.height / (this.maxPeriod - this.minPeriod + 1);
+    this.courseHeight = this.periodHeight - 4;
+    var courseYoffset = (this.courseHeight - this.periodHeight) / 2;
+
+    // Set coordinates
+    for (var array_index in this.courses) {
+      var course = this.courses[array_index];
+
+      if (course.period === false) {
+        continue;
+      }
+
+      var period = course.period - this.minPeriod;
+      course.y = this.periodHeight * period - courseYoffset;
+      course.x = Math.random() * this.height;
+    }
+    */
+
+
+  },
+
+  /**
+   * Loads courses from JSON data.
+   */
+  loadSkills: function(data) {
+    // Read JSON
+    for (var array_index in data) {
+      var rawData = data[array_index].skill;
+
+      // Create skill object
+      var skill = new GraphSkill(rawData.id, rawData.position, rawData.translated_name);
+      this.skills[rawData.id] = skill;
+
+      // Add skill to course
+      if (rawData.skillable_type == 'ScopedCourse') {
+        var course = this.courses[rawData.skillable_id];
+        if (!course) {
+          //console.log("Course "+rawData.skillable_id+" not found.");
+          continue;
+        }
+
+        course.addSkill(skill);
+        skill.setCourse(course);
+      }
+    }
+
+    // Set connections between skills
+    for (var array_index in data) {
+      rawData = data[array_index].skill;
+      skill = this.skills[rawData.id];
+
+      for (var array_index2 in rawData.strict_prereq_ids) {
+        var prereq = this.skills[rawData.strict_prereq_ids[array_index2]];
+
+        if (prereq) {
+          skill.addPrereq(prereq);
+        }
+      }
+    }
+  },
+
+  resetVisitedSkills: function() {
+    for (var array_index in this.skills) {
+      var skill = this.skills[array_index];
+      skill.visited = false;
+    }
+  },
+
+  initializeVisualization: function(courseId) {
+    var targetCourse = this.courses[courseId];
+
+    if (!targetCourse) {
+      //console.log("Target course "+courseId+" not found.");
+      return;
+    }
+
+    this.attachCourse(targetCourse);
+
+
+    // Run through course graph with DFS and add courses to level 1
+    //var levels = [targetCourse, {}];
+    var courses = {};
+    var stack = [targetCourse];
+
+    while (stack.length > 0) {
+      var course = stack.pop();
+
+      // Visit node
+      //levels[1][course.id] = course;
+      courses[course.id] = course;
+      course.visible = true;
+
+      // Add neighbors to stack
+      for (var array_index in course.prereqs) {
+        stack.push(course.prereqs[array_index]);
+      }
+    }
+
+    // Assign levels
+    var level = 0;
+    var nextLevel;
+
+    do {
+      nextLevel = false;
+
+      // Iterate through every course in the level. Push its prereqs to the next level.
+      for (var array_index in courses) {
+        var course = courses[array_index];
+
+        if (course.level == level) {
+          for (var array_index2 in course.prereqs) {
+            course.prereqs[array_index2].level = level + 1;
+            nextLevel = true;
+          }
+        }
+      }
+
+      level++;
+    } while(nextLevel);
+
+
+    // Create levels
+    var levels = level;
+    this.levels = Array(levels);
+    for (var i = 0; i < levels; i++) {
+      var level = new GraphLevel(i);
+      this.levels[i] = level;
+    }
+
+
+    // Add courses to levels and the view
+    var levelWidth = 600;
+
+    for (var array_index in courses) {
+      var course = courses[array_index];
+      this.attachCourse(course);
+      this.levels[course.level].addCourse(course);
+
+      var x = (levels - 1 - course.level) * levelWidth;
+      var y = 0;
+      course.setPosition(x, y);
+    }
+
+    // Calculate level heights
+    var maxHeight = 0;
+    for (var level_index in this.levels) {
+      var height = this.levels[level_index].updateHeight();
+      if (height > maxHeight) {
+        maxHeight = height;
+      }
+    }
+
+    for (var level_index in this.levels) {
+      this.levels[level_index].maxHeight = maxHeight;
+    }
+
+    targetCourse.y = (maxHeight + targetCourse.getElement(this).height()) / 2
+
+
+      /*
+      for (var course_index in courses) {
+        var course = courses[course_index];
+      }
+      */
+
+    // Set Y indices
+    for (var level_index = 0; level_index < this.levels.length; level_index++) {
+      var level = this.levels[level_index];
+      //var neighbor = this.levels[level - 1];
+
+      level.setYindicesBackwards();
+    }
+
+    // Updating positions
+    for (var course_index in courses) {
+      var course = courses[course_index];
+      course.updatePosition();
+    }
+
+    // Update svg size
+    this.paper.setSize($(document).width(), $(document).height());
+  },
+
+  resetHilights: function() {
+    $('#course-graph li').removeClass('hilight');
+    this.paper.clear();
+  },
+
+  createLine: function(x1, y1, x2, y2, w, color) {
+    var line = this.paper.path("M"+x1+" "+y1+"L"+x2+" "+y2);
+    line.attr("stroke", "#888");
+
+//     var line = document.createElementNS(this.svgNS, "line");
+//
+//     line.setAttributeNS(null, "x1", x1);
+//     line.setAttributeNS(null, "y1", y1);
+//     line.setAttributeNS(null, "x2", x2);
+//     line.setAttributeNS(null, "y2", y2);
+//     line.setAttributeNS(null, "stroke-width", w);
+//
+//     var color = "rgb(128,128,128)";
+//     line.setAttributeNS(null,"stroke",color);
+//
+//     this.svg.appendChild(line);
+  },
+
+  visualize: function() {
+    // Show:
+    // show the target course
+
+    // On click skill:
+    // take the starting node
+    // run DFS
+    //   mark visited nodes
+    //   assign levels
+    //   keep track of maximum width
+    // create divs and attach to DOM
+    // set div positions
+    // create edges
+  },
+
+  attachCourse: function(course) {
+    var courseCanvas = $('#course-graph');
+
+    var element = course.getElement(this);
+    courseCanvas.append(element);
+  },
+
+  /*
+  clickSkill: function(event) {
+    // Reset hilights
+    // Reset svg
+
+    // Run DFS
+    // hilight visited nodes
+    // draw edges
+  }
+  */
+
+};
+
+
+$(document).ready(function(){
+  var element = $('#course-graph');
+//   if (element.length > 0) {
+//     new PathViewer(element);
+//   }
+
+  skillGraphView.initialize();
+  var coursesPath = element.data('courses-path');
+  var skillsPath = element.data('skills-path');
+
+  skillGraphView.load(coursesPath, skillsPath);
+  skillGraphView.initializeVisualization(parseInt(element.data('course-id')));
+
+});

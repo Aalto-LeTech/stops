1a88ab7c5bf057dd462a625c8b6e5a7691feab9d
Joonas Saarinen <jpsaari5@cs.hut.fi>
2012-08-01 16:47:56 +0300

    Changed schedule view to load JS files using single asset manifest (schedule/schedule.js).

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require_tree ./templates/
+//= require o4-jquery-extensions
---
+++ app/assets/javascripts/schedule/schedule.js
@@ -0,0 +1,7 @@
+//= require raphael-min
+//= require schedule/period
+//= require schedule/course
+//= require schedule/courseinstance
+//= require schedule/plan
+//= require bootstrap
+

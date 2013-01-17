3fb2c66d1026486848d78fd760ff29517e05f26d
Tapio Auvinen <tsauvine@gmail.com>
2013-01-17 19:25:47 +0200

    Fixing timezone

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Ops
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Helsinki'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -44,7 +44,5 @@ module Ops
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end

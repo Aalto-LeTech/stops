750389f350e9edbb92e36d352f1c748cb154abf9
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-01-31 17:09:21 +0200

    Increasing asset version

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module Ops
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
   end
 end

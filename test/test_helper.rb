602e789fd508ffb356ba36e1af605427f86135ad
Joonas Saarinen <jpsaari5@cs.hut.fi>
2013-02-22 12:14:09 +0200

    Fix AuthenticatedTestHelper inclusion in test_helper.b
    The test couldn't be run since the AuthenticatedTestHelper module
    could not be found.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'authenticated_test_helper'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -8,6 +10,8 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
+
+  include AuthenticatedTestHelper
 
   # Add more helper methods to be used by all tests here...
 end

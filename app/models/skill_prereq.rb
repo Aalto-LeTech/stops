d461365826f709c1b5b1a176fed6f72119d76081
Joonas Saarinen <jpsaari5@cs.hut.fi>
2013-03-25 12:25:48 +0200

    Added 'requirement' attribute validation to SkillPrereq

--- app/models/skill_prereq.rb
+++ app/models/skill_prereq.rb
@@ -3,4 +3,5 @@ class SkillPrereq < ActiveRecord::Base
   belongs_to :skill
   belongs_to :prereq, :class_name => 'Skill', :foreign_key => 'prereq_id'
  
+  validates_numericality_of :requirement, :less_than_or_equal_to => 1
 end

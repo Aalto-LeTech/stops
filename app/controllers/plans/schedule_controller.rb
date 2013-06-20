2dd793063ae4ab501beab0e3cb1cdc75dc913da0
Samuel Marisa <samuel.marisa@cs.hut.fi>
2013-06-20 14:56:43 +0300

    debugging schedule planner

--- app/controllers/plans/schedule_controller.rb
+++ app/controllers/plans/schedule_controller.rb
@@ -5,40 +5,44 @@ class Plans::ScheduleController < PlansController
   before_filter :authenticate_user
 
   before_filter :load_plan
-  
+
   layout 'wide'
-  
-  
+
+
   def show
     @periods = @user.relevant_periods
     @curriculum = Curriculum.first # FIXME: @user.curriculum
-    
+
 
     @study_plan_courses = @user.study_plan.study_plan_courses
-    #@courses = Course.semesters(@user.courses) 
-    
+    #@courses = Course.semesters(@user.courses)
+
     #@credits = UserCourse.sum('credits', :include => :abstract_course, :conditions => ['user_id=?', @user.id])
-    
+
   end
-  
+
   def edit
   end
 
   def update
     # TODO: authentication
-    
-    params[:periods].each do |user_course_id, instance_id|
-      user_course = UserCourse.find(user_course_id)
-      next unless user_course
-        
-      user_course.course_instance_id = instance_id
-      user_course.save
+
+    puts "DBG::params: #{params}"
+
+    if params and params[:periods]
+      params[:periods].each do |user_course_id, instance_id|
+        user_course = UserCourse.find(user_course_id)
+        next unless user_course
+
+        user_course.course_instance_id = instance_id
+        user_course.save
+      end
     end
-    
+
 #     respond_to do |format|
 #       format.js { render :head => :ok }
 #     end
     render :text => 'ok'
   end
-  
+
 end
--- script/update_course_prereq_cache.rb
+++ script/update_course_prereq_cache.rb
@@ -5,3 +5,10 @@ ScopedCourse.all.each do |scoped_course|
 end
 
 
+#CoursePrereq.all.each do |course_prereqs_cache|
+#  course_prereqs_cache.requirement = 1
+#  course_prereqs_cache.save
+#  y course_prereqs_cache
+#end
+
+

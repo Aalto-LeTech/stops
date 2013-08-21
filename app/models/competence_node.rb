758f5187bbb61609ed5aedd255a442ed41ef77ee
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-08-21 17:36:58 +0300

    Added a method for merging CompetenceNodes

--- app/models/competence_node.rb
+++ app/models/competence_node.rb
@@ -9,4 +9,23 @@ class CompetenceNode < ActiveRecord::Base
            :dependent => :destroy,
            :order     => :position
 
+  # Moves skills from the other competence node to this node and deletes the other node
+  # other: CompetenceNode
+  def assimilate(other)
+    #Skill.where(:competence_node_id => other.id).update_all(:competence_node_id => self.id)
+    original_skill_count = self.skills.size
+    
+    # Remove prerequisite link from here to other
+    SkillPrereq.where(:skill_id => self.skill_ids, :prereq_id => other.skill_ids).destroy_all
+    
+    # Move skills of other to here
+    other.skills.each do |skill|
+      skill.competence_node_id = self.id
+      skill.position += original_skill_count
+      skill.save
+    end
+    
+    other.delete
+  end
+  
 end

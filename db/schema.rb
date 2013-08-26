6fa6947114b8c83ab64f2e9198a9ab61b973de5c
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-08-26 14:31:58 +0300

    Removed deep_cloneable gem

--- db/schema.rb
+++ db/schema.rb
@@ -170,8 +170,8 @@ ActiveRecord::Schema.define(:version => 20130816163842) do
     t.integer "study_plan_id",                           :null => false
     t.integer "scoped_course_id",                        :null => false
     t.integer "competence_ref_count", :default => 1,     :null => false
-    t.boolean "manually_added",       :default => false, :null => false
     t.integer "course_instance_id"
+    t.boolean "manually_added",       :default => false, :null => false
     t.integer "period_id"
     t.float   "credits",                                 :null => false
     t.integer "length"

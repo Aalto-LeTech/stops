744cb9c1acd8e08bf12338f7997f21c6cc65d9d4
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-09-10 17:12:35 +0300

    Bumping asset version number

--- config/application.rb
+++ config/application.rb
@@ -72,6 +72,6 @@ module Ops
     ]
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.18'
+    config.assets.version = '1.19'
   end
 end

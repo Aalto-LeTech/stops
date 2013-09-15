f145da5ee943501a2f58710aa01fac709164900e
Tapio Auvinen <tsauvine@gmail.com>
2013-09-15 19:08:45 +0300

    Expire assets

--- config/application.rb
+++ config/application.rb
@@ -72,6 +72,6 @@ module Ops
     ]
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.20'
+    config.assets.version = '1.21'
   end
 end

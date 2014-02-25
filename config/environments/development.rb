863c5d5941f6d1cfb412db37bba9a606b7a91699
Tapio Auvinen <tapio.auvinen@aalto.fi>
2014-02-25 16:15:04 +0200

    Don't send mails in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Ops::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :file
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
